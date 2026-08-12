4577537c39f0c59c29188caf7e41dc68022fc3f4
João de Lima <joaodelim090@gmail.com>
2026-08-11 22:14:18 -0300

    Add files via upload

--- src/aula04.jsx
+++ src/aula04.jsx
@@ -17,6 +17,10 @@ class Aula04 extends Component {
             source={ require('../assets/icon.png' )}
             style= { styles.icon }
         />
+        <Image 
+            source={{ uri: 'https://media.newyorker.com/photos/5909515e2179605b11ad2fc6/master/w_2560%2Cc_limit/godzilla-580.jpg' }}
+            style= { styles.icon }
+        />
 
       </View>
     );
@@ -29,6 +33,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  icon: {
+    width: 150,
+    height: 150,
+  }
+
 });
 
 export default Aula04;
---
+++ src/aula05.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Image, 
+    TouchableOpacity,
+} from 'react-native';
+
+class Aula05 extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View>
+
+            <Image
+                source={ require('../img/logo_barao.png')}
+                style={ styles.logo }
+            />
+
+            <Text style= { styles.label }>Nome: </Text>
+            <TextInput
+                style={ styles.input }
+                placeholder='    Informe seu nome: '
+            />
+
+            <Text style= { styles.label }>Email: </Text>
+            <TextInput
+                style={ styles.input }
+                placeholder='    Informe seu e-mail: '
+            />
+
+            <TouchableOpacity style={ styles.botao }>
+                <Text style={ styles.textoBotao }>Entrar</Text>
+            </TouchableOpacity>
+
+            <Text style={ styles.texto1 }>Não tem conta... 
+                <Text style={ styles.texto2 }>Cadastre-se!</Text>
+            </Text>
+
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+
+  input: {
+    width: 250,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 16,
+    backgroundColor: 'white'
+  },
+
+  label:{
+    fontSize: 16,
+    marginBottom: 5,
+    color: 'white'
+    // width:200
+  },
+
+  logo: {
+    width: 120,
+    height: 120,
+    alignSelf: 'center',
+    marginBottom: 70
+  },
+
+  botao: {
+    backgroundColor: 'green',
+    width: 250,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 30,
+  },
+
+  textoBotao: {
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  texto1: {
+    color: 'white'
+  },
+
+  texto2: {
+    color: 'blue'
+  },
+
+});
+
+export default Aula05;
