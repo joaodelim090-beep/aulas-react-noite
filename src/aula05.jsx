0937e1c308a3f3fdfdb6884b1ede4e9d8e65f2ec
João de Lima <joaodelim090@gmail.com>
2026-08-18 22:12:31 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'expo-checkbox';
 import { 
     View, 
     Text, 
@@ -8,12 +9,28 @@ import {
     TouchableOpacity,
 } from 'react-native';
 
+
 class Aula05 extends Component {
+
+  //Construtor - Forma tradicional de inicializar estado ( dados )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nome: '',
+      email: '', 
+      lembrarSenha: false
+    };
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
 
         <View>
+
+            <Text style={ styles.login }>LOGIN</Text>
 
             <Image
                 source={ require('../img/logo_barao.png')}
@@ -32,16 +49,28 @@ class Aula05 extends Component {
                 placeholder='    Informe seu e-mail: '
             />
 
+            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Checkbox
+                      value={this.state.lembrarSenha }
+                      onValueChange= { (valor) => this.setState({ lembrarSenha: valor})}
+                      color={ this.state.lembrarSenha ? '#4630EB': undefined}
+                    />
+                    <Text style={{ color: 'white', marginLeft: 3, fontSize: 12}}>Lembrar senha!</Text>
+                </View>
+                <Text style={{ color: '#39FF14', fontSize: 12}}>Esqueceu a senha?</Text>
+            </View>
+
             <TouchableOpacity style={ styles.botao }>
                 <Text style={ styles.textoBotao }>Entrar</Text>
             </TouchableOpacity>
 
-            <Text style={ styles.texto1 }>Não tem conta... 
+            <Text style={ styles.texto1 }>Não tem conta? 
                 <Text style={ styles.texto2 }>Cadastre-se!</Text>
             </Text>
 
         </View>
-
+ 
       </View>
     );
   }
@@ -52,7 +81,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black'
+    backgroundColor: 'black',
   },
 
   input: {
@@ -63,21 +92,30 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 15,
     fontSize: 16,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
 
   label:{
     fontSize: 16,
     marginBottom: 5,
-    color: 'white'
+    color: 'white',
     // width:200
+  },
+
+  login: {
+    color: '#39FF14',
+    textAlign: 'center',
+    marginLeft: 10,
+    fontSize: 50,
+    fontWeight: 'bold'
   },
 
   logo: {
     width: 120,
     height: 120,
     alignSelf: 'center',
-    marginBottom: 70
+    marginBottom: 20,
+    marginTop: 20,
   },
 
   botao: {
@@ -94,11 +132,17 @@ const styles = StyleSheet.create({
   },
 
   texto1: {
-    color: 'white'
+    color: 'white',
+    fontSize: 15,
+    marginTop: 6,
+    textAlign: 'center',
   },
 
   texto2: {
-    color: 'blue'
+    color: '#39FF14',
+    //marginRight: 20,
+    marginLeft: 10,
+    fontSize: 15,
   },
 
 });
---
+++ src/aula06.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Aula06 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Aula06;
